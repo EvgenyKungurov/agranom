1c062ea3c3565c1478df6f009c5b032beae0585a
Evgeny Kungurov <evgenii.kungurov@gmail.com>
2016-09-30 13:38:10 +0000

    Add rspec rails_admin

---
+++ spec/features/rails_admin/sign_in_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature 'Rails admin', type: :feature do
+  def sign_in(user, role = nil)
+    user.add_role :admin if role == :admin
+    visit '/sign_in'
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_button 'Log in'
+  end
+
+  let!(:user) { FactoryGirl.create :user }
+  let!(:article) { FactoryGirl.create :article }
+  let!(:article_params) { FactoryGirl.attributes_for(:article) }
+
+  describe 'GET #rails_admin' do
+    it 'should not be available for users' do
+      sign_in(user)
+      visit rails_admin_path
+      expect(current_path).to eq root_path
+    end
+
+    it 'should not be available for anonymous' do
+      visit new_article_path
+      expect(current_path).to eq new_user_session_path
+    end
+
+    it 'can only admin' do
+      sign_in(user, :admin)
+      visit rails_admin_path
+      expect(current_path).to eq rails_admin_path
+    end
+  end
+end
